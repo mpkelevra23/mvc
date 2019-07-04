0cb83b8657e073bb263a66e75299dd5ff8fba416
mpkelevra23 <mpkelevra23@gmail.com>
2019-07-04 12:05:02 +0300

    Добавил класс Db для подключения к бд, Добавил db_params, для подключения к бд.

---
+++ app/web/config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'mvc',
+    'charset' => 'utf8',
+    'user' => 'admin',
+    'password' => '123456',
+];
---
+++ app/web/models/Db.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Db
+{
+    /**
+     * @return PDO
+     */
+    public static function getConnection()
+    {
+        $paramsPath = ROOT . '/config/db_params.php';
+        $params = include($paramsPath);
+
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']};charset={$params['charset']}";
+        $db = new PDO($dsn, $params['user'], $params['password']);
+
+        return $db;
+    }
+
+}
