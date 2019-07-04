398b0bd1e0dd3c1457ad2fabe4ed4e221b1b0add
mpkelevra23 <mpkelevra23@gmail.com>
2019-07-04 12:02:16 +0300

    Добавил комментарии в класс Router. Добавил передачу аргументов в action контроллера

--- app/web/components/Router.php
+++ app/web/components/Router.php
@@ -5,6 +5,9 @@ class Router
 {
     private $routes;
 
+    /**
+     * Router constructor.
+     */
     public function __construct()
     {
         $routesPath = ROOT . '/config/routes.php';
@@ -12,7 +15,7 @@ class Router
     }
 
     /**
-     * Получаем строку запроса
+     * Get URI string
      * @return string
      */
     private function getURI()
@@ -24,24 +27,41 @@ class Router
 
     public function run()
     {
+        //Получаем URI, который ввёл пользователь
         $uri = $this->getURI();
 
+        //
         foreach ($this->routes as $uriPattern => $path) {
+            //Сравниваем $uriPattern и $path
             if (preg_match("~$uriPattern~", $uri)) {
-                $segments = explode('/', $path);
+
+                //Получаем внутрений путь из указанного в URI, согласно правилу
+                $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+
+                //Определяем контроллер, action, параметры
+                $segments = explode('/', $internalRoute);
 
                 $controllerName = ucfirst(array_shift($segments) . 'Controller');
 
                 $actionName = 'action' . ucfirst(array_shift($segments));
 
+                $parameters = $segments;
+
+
+                //Получаем файл класс-контроллера
                 $controllerFile = ROOT . '/controllers/' . $controllerName . '.php';
 
+                //Если файл существует, то подключаем его
                 if (file_exists($controllerFile)) {
                     require_once($controllerFile);
                 }
 
+                //Создаём объект контроллера
                 $controllerObject = new $controllerName;
-                $result = $controllerObject->$actionName();
+
+                //Вызываем метод (action), передаём параметры
+                $result = call_user_func_array(array($controllerObject, $actionName), $parameters);
+
                 if ($result != null) {
                     break;
                 }
