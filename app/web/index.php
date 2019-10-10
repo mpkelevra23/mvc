960d9fe6acb4b02132d69a8d806db9551db2d26c
mpkelevra23 <mpkelevra23@gmail.com>
2019-10-10 22:58:51 +0300

    Мелкие изменения

--- app/web/index.php
+++ app/web/index.php
@@ -12,7 +12,7 @@ error_reporting(E_ALL);
  */
 define('ROOT', dirname(__FILE__));
 require_once(ROOT . '/components/Router.php');
-include_once(ROOT . '/models/Db.php');
+include_once(ROOT . '/components/Db.php');
 
 /*
  *  Вызов класса Router
